3516de184c4e8bd86724c6650d33a90a53a2b9ed
Lisji <twn0958983620@gmail.com>
2022-08-12 19:50:05 +0800

    :sparkles: add router in project and component homepage

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import "./icons";
+import "./icons"
+import router from './router/index.js'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
